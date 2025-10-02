a9164ebec06efa70ba5dfbc65fef38502336bbf5
Minusliit <it24102269@my.sliit.lk>
2025-10-02 13:53:04 +0530

    Add migration state XML files and enhance dashboard UI with improved styles and hover effects

--- src/main/java/com/vims/security/SecurityManagementSystemApplication.java
+++ src/main/java/com/vims/security/SecurityManagementSystemApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityManagementSystemApplication {
+public class  SecurityManagementSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecurityManagementSystemApplication.class, args);
     }
